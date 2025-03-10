e23d750bf3f81b12c08586f704f274e940ac58a6
Morten Lysgaard <morten@lysgaard.no>
2025-03-10 13:06:00 +0100

    dummy log on query controls

--- src/device.rs
+++ src/device.rs
@@ -120,6 +120,7 @@ impl Device {
 
     /// Returns the supported controls for a device such as gain, focus, white balance, etc.
     pub fn query_controls(&self) -> io::Result<Vec<control::Description>> {
+        log::error!("Querying controls");
         let mut controls = Vec::new();
         unsafe {
             let mut v4l2_ctrl: v4l2_query_ext_ctrl = mem::zeroed();
